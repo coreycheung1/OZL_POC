3739d0b54c60b236e16b2ac5c6d97c15829afd74
coreyc@jumbointeractive.com <coreyc@jumbointeractive.com>
2021-12-24 02:45:09 +0000

    stg_checkout model query

--- models/2_staging/ozl/stg_checkout.sql
+++ models/2_staging/ozl/stg_checkout.sql
@@ -1,14 +1,39 @@
-with checkout_ozl_n3 as (
+with n3 as (
     
     select *
-    from {{ source('ozl_node_3', 'checkout') }}
+    from {{ ref('base_checkout_ozl_n3') }}
 ),
 
-with checkout_ozl_n4 as (
+n4 as (
     
     select *
-    from {{ source('ozl_node_4', 'checkout') }}
+    from {{ ref('base_checkout_ozl_n4') }}
+),
+
+checkout_unioned as (
+
+    select * from n3
+    union all
+    select * from n4
+),
+
+channel as (
+
+    select * from {{ ref('stg_channel') }}
+),
+
+final as (
+
+    select 
+        checkout_unioned.checkout_id,
+        checkout_unioned.checkout_timestamp,
+        checkout_unioned.customer_id,
+        channel.channel_name as channel,
+        checkout_unioned.checkout_sale_data,
+        checkout_unioned.source
+    from checkout_unioned
+    join channel on checkout_unioned.channel_id = channel.channel_id
 )
 
 select *
-from channel
+from final
