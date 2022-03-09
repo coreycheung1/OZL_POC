9cc977de10b703dfea28012448bb62d9f0d2d147
Corey Cheung <coreyc@jumbointeractive.com>
2022-03-09 18:03:26 +1000

    added account_transaction and type models

---
+++ models/1_staging/ozl/stg_account_transaction_type.sql
@@ -0,0 +1,11 @@
+with account_transaction_type as (
+    
+    select 
+        account_transaction_type_id,
+        account_transaction_type_name,
+        account_transaction_type_description
+    from {{ source('ozl_node_3', 'account_transaction_type') }}
+)
+
+select *
+from account_transaction_type
