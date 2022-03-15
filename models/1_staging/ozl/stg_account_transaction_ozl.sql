15017f7b7bb78e701c4785d34e67ba52340bb8b3
Corey Cheung <coreyc@jumbointeractive.com>
2022-03-15 14:20:46 +1000

    changed stg acc tran

--- models/1_staging/ozl/stg_account_transaction_ozl.sql
+++ models/1_staging/ozl/stg_account_transaction_ozl.sql
@@ -3,6 +3,19 @@ with account_transaction_unioned as (
     {{ union_node_sources('ozl', 4, 'account_transaction') }}
 ),
 
+account_transaction_cleaned as (
+
+    select
+        source,
+        concat(account_transaction_id, '_', source) as account_transaction_id,
+        convert_timezone('UTC', 'Australia/Brisbane', account_transaction_timestamp) as account_transaction_timestamp,
+        concat(account_id, '_', database) as account_id,
+        try_parse_json(transaction_reason) as transaction_reason,
+        transaction_amount,
+        account_transaction_type_id
+    from account_transaction_unioned        
+),
+
 account_transaction_type as (
 
     select * from {{ ref('stg_account_transaction_type_ozl') }}
@@ -11,16 +24,16 @@ account_transaction_type as (
 final as (
 
     select
-        atu.source,
-        concat(atu.account_transaction_id, '_', source) as account_transaction_id,
-        convert_timezone('UTC', 'Australia/Brisbane', atu.account_transaction_timestamp) as account_transaction_timestamp,
-        concat(atu.account_id, '_', database) as account_id,
-        try_parse_json(atu.transaction_reason) as transaction_reason,
-        atu.transaction_amount,
-        atu.account_transaction_type_id,
+        at.source,
+        at.account_transaction_id,
+        at.account_transaction_timestamp,
+        at.account_id,
+        at.transaction_reason,
+        at.transaction_amount,
+        at.account_transaction_type_id,
         att.account_transaction_type_name as account_transaction_type
-    from account_transaction_unioned atu
-    join account_transaction_type att on atu.account_transaction_type_id = att.account_transaction_type_id
+    from account_transaction_cleaned at
+    join account_transaction_type att on at.account_transaction_type_id = att.account_transaction_type_id
 )
 
 select *
