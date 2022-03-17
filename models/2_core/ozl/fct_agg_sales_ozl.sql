481b4e55396e1248f8b54da6795a0043d69e0428
Corey Cheung <coreyc@jumbointeractive.com>
2022-03-17 14:41:57 +1000

    fct agg sales for real

--- models/2_core/ozl/fct_agg_sales_ozl.sql
+++ models/2_core/ozl/fct_agg_sales_ozl.sql
@@ -1,26 +1,47 @@
-with sales_ordered as (
+with sales as (
 
-    select 
-        *,
-        row_number() over (partition by customer_id order by sale_timestamp asc, TTV desc) ords 
+    select *
     from {{ ref('fct_sales_ozl') }}
     where status in ('SYNDICATE_SHARE_PURCHASED', 'SYNDICATE_SHARE_PAID', 'TICKET_PURCHASED', 'TICKET_PAID')
 ),
 
 first_sale as (
 
+    select *
+    from {{ ref('fct_first_sale_ozl') }}
+),
+
+total_TTV as (
+
     select
         customer_id,
-        sale_id as first_sale_id,
-        sale_timestamp as first_sale_timestamp,
-        checkout_id as first_checkout_id,
-        draw_id as first_draw_id,
-        game_offer_id as first_game_offer_id,
-        channel as first_channel,
-        checkout_sale_data as first_checkout_sale_data
-    from sales_ordered
-    where ords = 1 -- first sale
+        sum(TTV) as total_TTV
+    from sales
+    group by 1
+),
+
+_180_day_TTV as (
+
+    select 
+        s.customer_id,
+        sum(TTV) as _180_day_TTV
+    from sales s
+    join first_sale fs on s.customer_id = fs.customer_id
+    where datediff('day', fs.first_sale_timestamp, s.sale_timestamp) <= 180
+    and first_sale_timestamp <= current_date - interval '180days'
+    group by 1
+),
+
+final as (
+
+    select 
+        t.customer_id,
+        total_TTV,
+        _180_day_TTV
+    from total_TTV as t
+    join _180_day_TTV as _180 on t.customer_id = _180.customer_id
+
 )
 
-select * 
-from first_sale
+select *
+from final
