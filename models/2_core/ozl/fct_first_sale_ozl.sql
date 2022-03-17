f773ffca806d8fd024e1ba6fa9ae920d31c47fcd
Corey Cheung <coreyc@jumbointeractive.com>
2022-03-17 14:23:07 +1000

    agg sales model

--- models/2_core/ozl/fct_first_sale_ozl.sql
+++ models/2_core/ozl/fct_first_sale_ozl.sql
@@ -1,15 +1,6 @@
-with sales_ordered as (
+with first_sale as (
 
     select 
-        *,
-        row_number() over (partition by customer_id order by sale_timestamp asc, TTV desc) ords 
-    from {{ ref('fct_sales_ozl') }}
-    where status in ('SYNDICATE_SHARE_PURCHASED', 'SYNDICATE_SHARE_PAID', 'TICKET_PURCHASED', 'TICKET_PAID')
-),
-
-first_sale as (
-
-    select
         customer_id,
         sale_id as first_sale_id,
         sale_timestamp as first_sale_timestamp,
@@ -17,9 +8,11 @@ first_sale as (
         draw_id as first_draw_id,
         game_offer_id as first_game_offer_id,
         channel as first_channel,
-        checkout_sale_data as first_checkout_sale_data
-    from sales_ordered
-    where ords = 1 -- first sale
+        checkout_sale_data as first_checkout_sale_data,
+        row_number() over (partition by customer_id order by sale_timestamp asc, TTV desc) ords 
+    from {{ ref('fct_sales_ozl') }}
+    where status in ('SYNDICATE_SHARE_PURCHASED', 'SYNDICATE_SHARE_PAID', 'TICKET_PURCHASED', 'TICKET_PAID')
+    qualify ords = 1
 )
 
 select * 
