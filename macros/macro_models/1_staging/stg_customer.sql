647644d92a33fe6d6682976e27f9518e17f85705
coreyc@jumbointeractive.com <coreyc@jumbointeractive.com>
2022-04-11 23:33:30 +0000

    changed stg and dim customer to macros

---
+++ macros/macro_models/1_staging/stg_customer.sql
@@ -0,0 +1,56 @@
+{%- macro stg_customer(pbj) -%}
+    
+with customer_unioned as (
+
+    {{ union_node_sources(pbj, 4, 'customer')}}
+),
+
+customer_cleaned as (
+
+    select
+        source,
+        customer_id,
+        concat(account_id, '_', source) as account_id,
+        firstname,
+        lastname,
+        dob,
+        parse_json(address) as address, -- cast to json
+        suburb,
+        postcode,
+        state,
+        country,
+        phone,
+        email,
+        convert_timezone('UTC', 'Australia/Brisbane', customer_timestamp) as signup_timestamp
+    from customer_unioned
+),
+
+final as (
+
+    select
+        source,
+        customer_id,
+        account_id,
+        firstname,
+        lastname,
+        dob,
+        address,
+        suburb,
+        postcode,
+        state,
+        country,
+        phone,
+        email,
+        signup_timestamp
+    from customer_cleaned
+)
+
+select * 
+from final
+
+{%- endmacro -%}
+
+
+
+
+
